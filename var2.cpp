cbb7345ec75bdab38dbc168a1ffa1a503205b444
athl64 <athl64@bigmir.net>
2013-09-19 23:26:36 +0300

    filled 2-nd vaariant; required testing of correct; required editing last task in 1-st variant

--- var2.cpp
+++ var2.cpp
@@ -40,33 +40,33 @@ void var2::check()
         tasks2[ii]=0;
     }
 
-    if((ui->lineEdit->text()=="1" && ui->lineEdit_2->text()=="" && ui->lineEdit_3->text()=="2" && ui->lineEdit_4->text()=="3" && ui->lineEdit_6->text()=="1" && ui->lineEdit_7->text()=="" && ui->lineEdit_5->text()=="" && ui->radioButton->isChecked()==false) || (ui->lineEdit->text()=="1" && ui->lineEdit_2->text()=="1" && ui->lineEdit_3->text()=="" && ui->lineEdit_4->text()=="" && ui->lineEdit_6->text()=="" && ui->lineEdit_7->text()=="2" && ui->lineEdit_5->text()=="3" && ui->radioButton->isChecked()==false)) {
+    if((ui->lineEdit->text()=="25" && ui->lineEdit_2->text()=="-2" && ui->lineEdit_3->text()=="" && ui->lineEdit_4->text()=="" && ui->lineEdit_6->text()=="-" && ui->lineEdit_7->text()=="1" && ui->lineEdit_5->text()=="3" && ui->radioButton->isChecked()==false) || (ui->lineEdit->text()=="25" && ui->lineEdit_2->text()=="-" && ui->lineEdit_3->text()=="1" && ui->lineEdit_4->text()=="3" && ui->lineEdit_6->text()=="-2" && ui->lineEdit_7->text()=="" && ui->lineEdit_5->text()=="" && ui->radioButton->isChecked()==false)) {
         mark2++;
         tasks2[0]=1;
     };
-    if((ui->lineEdit_8->text()=="-19" && ui->lineEdit_11->text()=="" && ui->lineEdit_12->text()=="" && ui->lineEdit_9->text()=="" && ui->lineEdit_10->text()=="" && ui->lineEdit_14->text()=="" && ui->lineEdit_13->text()=="" && ui->radioButton_2->isChecked()==true) || (ui->lineEdit_8->text()=="-19" && ui->lineEdit_11->text()=="" && ui->lineEdit_12->text()=="" && ui->lineEdit_9->text()=="" && ui->lineEdit_10->text()=="" && ui->lineEdit_14->text()=="" && ui->lineEdit_13->text()=="" && ui->radioButton_2->isChecked()==true)) {
+    if((ui->lineEdit_8->text()=="0" && ui->lineEdit_11->text()=="6" && ui->lineEdit_12->text()=="" && ui->lineEdit_9->text()=="" && ui->lineEdit_10->text()=="6" && ui->lineEdit_14->text()=="" && ui->lineEdit_13->text()=="" && ui->radioButton_2->isChecked()==false) || (ui->lineEdit_8->text()=="0" && ui->lineEdit_11->text()=="6" && ui->lineEdit_12->text()=="" && ui->lineEdit_9->text()=="" && ui->lineEdit_10->text()=="6" && ui->lineEdit_14->text()=="" && ui->lineEdit_13->text()=="" && ui->radioButton_2->isChecked()==false)) {
         mark2++;
         tasks2[1]=1;
     };
-    if((ui->lineEdit_15->text()=="0" && ui->lineEdit_18->text()=="-5" && ui->lineEdit_19->text()=="" && ui->lineEdit_16->text()=="" && ui->lineEdit_17->text()=="-5" && ui->lineEdit_21->text()=="" && ui->lineEdit_20->text()=="" && ui->radioButton_3->isChecked()==false) || (ui->lineEdit_15->text()=="0" && ui->lineEdit_18->text()=="-5" && ui->lineEdit_19->text()=="" && ui->lineEdit_16->text()=="" && ui->lineEdit_17->text()=="-5" && ui->lineEdit_21->text()=="" && ui->lineEdit_20->text()=="" && ui->radioButton->isChecked()==false)) {
+    if((ui->lineEdit_15->text()=="-47" && ui->lineEdit_18->text()=="" && ui->lineEdit_19->text()=="" && ui->lineEdit_16->text()=="" && ui->lineEdit_17->text()=="" && ui->lineEdit_21->text()=="" && ui->lineEdit_20->text()=="" && ui->radioButton_3->isChecked()==true) || (ui->lineEdit_15->text()=="-47" && ui->lineEdit_18->text()=="" && ui->lineEdit_19->text()=="" && ui->lineEdit_16->text()=="" && ui->lineEdit_17->text()=="" && ui->lineEdit_21->text()=="" && ui->lineEdit_20->text()=="" && ui->radioButton->isChecked()==true)) {
         mark2++;
         tasks2[2]=1;
     };
-    if((ui->lineEdit_22->text()=="121" && ui->lineEdit_25->text()=="" && ui->lineEdit_26->text()=="2" && ui->lineEdit_23->text()=="3" && ui->lineEdit_24->text()=="-" && ui->lineEdit_28->text()=="1" && ui->lineEdit_27->text()=="4" && ui->radioButton->isChecked()==false) || (ui->lineEdit_22->text()=="121" && ui->lineEdit_25->text()=="-" && ui->lineEdit_26->text()=="1" && ui->lineEdit_23->text()=="4" && ui->lineEdit_24->text()=="" && ui->lineEdit_28->text()=="2" && ui->lineEdit_27->text()=="3" && ui->radioButton->isChecked()==false)) {
+    if((ui->lineEdit_22->text()=="169" && ui->lineEdit_25->text()=="-2" && ui->lineEdit_26->text()=="" && ui->lineEdit_23->text()=="" && ui->lineEdit_24->text()=="" && ui->lineEdit_28->text()=="5" && ui->lineEdit_27->text()=="4" && ui->radioButton->isChecked()==false) || (ui->lineEdit_22->text()=="169" && ui->lineEdit_25->text()=="" && ui->lineEdit_26->text()=="5" && ui->lineEdit_23->text()=="4" && ui->lineEdit_24->text()=="-2" && ui->lineEdit_28->text()=="" && ui->lineEdit_27->text()=="" && ui->radioButton->isChecked()==false)) {
         mark2++;
         tasks2[3]=1;
     };
     //end of 1-st task
-    if((ui->lineEdit_29->text()=="64" && ui->lineEdit_30->text()=="1" && ui->lineEdit_33->text()=="1" && ui->lineEdit_35->text()=="2" && ui->lineEdit_31->text()=="-" && ui->lineEdit_32->text()=="1" && ui->lineEdit_34->text()=="2" && ui->radioButton->isChecked()==false) || (ui->lineEdit_29->text()=="64" && ui->lineEdit_30->text()=="-" && ui->lineEdit_33->text()=="1" && ui->lineEdit_35->text()=="2" && ui->lineEdit_31->text()=="1" && ui->lineEdit_32->text()=="1" && ui->lineEdit_34->text()=="2" && ui->radioButton->isChecked()==false)) {
+    if((ui->lineEdit_29->text()=="36" && ui->lineEdit_30->text()=="1" && ui->lineEdit_33->text()=="" && ui->lineEdit_35->text()=="" && ui->lineEdit_31->text()=="7" && ui->lineEdit_32->text()=="" && ui->lineEdit_34->text()=="" && ui->radioButton->isChecked()==false) || (ui->lineEdit_29->text()=="36" && ui->lineEdit_30->text()=="7" && ui->lineEdit_33->text()=="" && ui->lineEdit_35->text()=="" && ui->lineEdit_31->text()=="1" && ui->lineEdit_32->text()=="" && ui->lineEdit_34->text()=="" && ui->radioButton->isChecked()==false)) {
         mark2++;
         tasks2[4]=1;
     };
-    if((ui->lineEdit_40->text()=="1024" && ui->lineEdit_38->text()=="3" && ui->lineEdit_42->text()=="4" && ui->lineEdit_39->text()=="9" && ui->lineEdit_36->text()=="-" && ui->lineEdit_37->text()=="1" && ui->lineEdit_41->text()=="9" && ui->radioButton->isChecked()==false) || (ui->lineEdit_40->text()=="1024" && ui->lineEdit_38->text()=="-" && ui->lineEdit_42->text()=="1" && ui->lineEdit_39->text()=="9" && ui->lineEdit_36->text()=="3" && ui->lineEdit_37->text()=="4" && ui->lineEdit_41->text()=="9" && ui->radioButton->isChecked()==false)) {
+    if((ui->lineEdit_40->text()=="49" && ui->lineEdit_38->text()=="" && ui->lineEdit_42->text()=="2" && ui->lineEdit_39->text()=="5" && ui->lineEdit_36->text()=="-1" && ui->lineEdit_37->text()=="" && ui->lineEdit_41->text()=="" && ui->radioButton->isChecked()==false) || (ui->lineEdit_40->text()=="49" && ui->lineEdit_38->text()=="-1" && ui->lineEdit_42->text()=="" && ui->lineEdit_39->text()=="" && ui->lineEdit_36->text()=="" && ui->lineEdit_37->text()=="2" && ui->lineEdit_41->text()=="5" && ui->radioButton->isChecked()==false)) {
         mark2++;
         tasks2[5]=1;
     };
     //end of 2-nd task
-    if((ui->lineEdit_43->text()=="10" && ui->lineEdit_44->text()=="5" && ui->lineEdit_45->text()=="6" && ui->lineEdit_47->text()=="32" && ui->lineEdit_46->text()=="1" && ui->lineEdit_48->text()=="2") || (ui->lineEdit_43->text()=="32" && ui->lineEdit_44->text()=="1" && ui->lineEdit_45->text()=="2" && ui->lineEdit_47->text()=="10" && ui->lineEdit_46->text()=="5" && ui->lineEdit_48->text()=="6")) {
+    if((ui->lineEdit_43->text()=="8" && ui->lineEdit_44->text()=="" && ui->lineEdit_45->text()=="" && ui->lineEdit_47->text()=="14" && ui->lineEdit_46->text()=="" && ui->lineEdit_48->text()=="") || (ui->lineEdit_43->text()=="14" && ui->lineEdit_44->text()=="" && ui->lineEdit_45->text()=="" && ui->lineEdit_47->text()=="8" && ui->lineEdit_46->text()=="" && ui->lineEdit_48->text()=="")) {
         mark2++;
         tasks2[6]=1;
     };
